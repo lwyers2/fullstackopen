d9d0766c81068fa9a61841c838c4226b95fdf5f8
lwyers <lwyers2@gmail.com>
2024-08-10 21:04:24 +0100

    Exercise 1 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  //const definitions
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 =7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
+    <div>
+      
+      <Header course={course}/>
+     
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+
+    </div>
+  )
+}
+
+const Header = (course) => {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>{course.course}</h1>
+    </>
+  )
+}
+
+const Content = (part) => {
+  return (
+    <>
+    <p>{part.part} {part.exercises}</p>
+    </>
+  )
+}
+
+const Total = (total) => {
+  return (
+    <>
+    <p>Number of exercises {total.exercises1+total.exercises2+total.exercises3}</p>
     </>
   )
 }
