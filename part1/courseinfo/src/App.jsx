3564a21ee4b166d6b3d2539fc42415152a2c05d6
lwyers <lwyers2@gmail.com>
2024-08-11 11:07:16 +0100

    Merging Exercise 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,19 +2,25 @@ const App = () => {
 
   //const definitions
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 =7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 10
+  }
 
   return (
     <div>
       
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
 
     </div>
   )
@@ -31,9 +37,9 @@ const Header = (course) => {
 const Content = (part) => {
   return (
     <div>
-    <Part part={part.part1} exercises={part.exercises1}/>
-    <Part part={part.part2} exercises={part.exercises2}/>
-    <Part part={part.part3} exercises={part.exercises3}/>
+    <Part part={part.part1.name} exercises={part.part1.exercises}/>
+    <Part part={part.part2.name} exercises={part.part2.exercises}/>
+    <Part part={part.part3.name} exercises={part.part3.exercises}/>
     </div>
   )
 }
@@ -49,7 +55,7 @@ const Part = (part) => {
 const Total = (total) => {
   return (
     <>
-    <p>Number of exercises {total.exercises1+total.exercises2+total.exercises3}</p>
+    <p>Number of exercises {total.part1.exercises+total.part2.exercises+total.part3.exercises}</p>
     </>
   )
 }
