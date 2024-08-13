3f4e9e3d8b9e7522c6c3e288b19b1644900318c5
lwyers <lwyers2@gmail.com>
2024-08-13 20:41:08 +0100

    Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -21,21 +21,29 @@ const App = () => {
     console.log(props)
     
     if(props.all == 0){
-      return (<StatisticLine text="No feedback given"/>)
+      return (<p>No feedback given</p>)
     }
     return (
     <div>
+      <table>
     <StatisticLine text="good" value={props.good}/>  
     <StatisticLine text="neutral" value={props.neutral}/>  
     <StatisticLine text="bad" value={props.bad}/>  
     <StatisticLine text="all" value={props.all}/>  
     <StatisticLine text="average" value={props.average}/>  
     <StatisticLine text="positive" value={props.positive} delimiter={props.delimiter}/>  
+    </table>
     </div>
   )}
 
   const StatisticLine = props => (
-    <p>{props.text} {props.value} {props.delimiter}</p>
+    <tbody>
+    <tr>
+    <td>{props.text}</td> 
+    <td>{props.value}</td>
+    <td>{props.delimiter}</td>
+    </tr>
+    </tbody>
   )
 
 
