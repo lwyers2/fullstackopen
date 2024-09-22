3653cdce76f9d3a9cac64aa11d5583fd5a04b2a6
lwyers <lwyers2@gmail.com>
2024-09-22 10:47:06 +0100

    Exercises 3.5 and 3.6

--- part3/index.js
+++ part3/index.js
@@ -55,6 +55,50 @@ app.delete('/api/persons/:id' , (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+   
+    Id = Math.floor(Math.random() * 3650000)
+
+    return String(Id)
+  }
+
+app.post('/api/persons', (request, response) => {
+
+    const number = request.body.number
+    const name = request.body.name
+
+    
+    if(!number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if(!name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if(persons.find(person => person.name === name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    
+    const person = {
+        id: generateId(),
+        name: name,
+        number: number
+    }
+
+    persons = persons.concat(person)
+    response.json(persons)
+    
+})
+
+
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
