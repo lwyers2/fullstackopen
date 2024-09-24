cbcc45abfda24bc78491388855f34701fa839a9b
lwyers <lwyers2@gmail.com>
2024-09-24 21:38:17 +0100

    Exercise 3.11

--- part3/index.js
+++ part3/index.js
@@ -24,6 +24,11 @@ let persons =
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    {
+        "id": "5",
+        "name": "Michael Johnstone", 
+        "number": "59865515"
     }
 ]
 
