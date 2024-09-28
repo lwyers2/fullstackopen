ebc3fb2c9acb11a18db1b247ad0cb0152af700ec
lwyers2 <lwyers2@gmail.com>
2024-09-28 13:55:54 +0100

    Exercise 3.20

--- part3/models/person.js
+++ part3/models/person.js
@@ -18,12 +18,19 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
         name: {
             type: String,
-            minLength: [3, `Path \`name\` (\`{VALUE}\`)is shorter than the minimum allowed length(3)`],
-            required: true
+            minLength: [3, `Path \`name\` (\`{VALUE}\`)is shorter than the minimum allowed length (3)`],
+            required: [true, `Path \`name\` (\`\`)should be populated, please valid name`] 
         },
         number: {
             type: String,
-            required: true
+            minlength: [8, `Path \`number\` (\`{VALUE}\`) is shorter than the minimum allowed length (8)`],
+            validate: {
+                validator: function(v) {
+                    return /^\d{2,3}-\d+$/.test(v);
+                },
+                message: props => `Path \`number\` (\`${props.value}\`) is not a valid number`
+            },
+            required: [true, `Path \`number\` (\`\`)should be populated, please enter valid number`]
         }
         })
 
