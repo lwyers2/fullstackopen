99a24f1346203528d17327668c48e22cfd2243a0
lwyers2 <lwyers2@gmail.com>
2024-09-29 13:00:47 +0100

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,20 @@
 const dummy = (blogs) => {
     return (!blogs) ?  1 :  1
-    
+}
+
+const totalLikes = (blogs) => {
+    if(!blogs || blogs.length ===0) {
+        return 0
+    } else {
+        const sum = blogs.reduce (
+            (accummulator, currentValue) => accummulator + (currentValue.likes || 0), 0 ,
+        )
+        
+        return sum
+    }
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
