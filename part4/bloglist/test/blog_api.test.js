87ebea8f948d3d2e8b5b15f4463766f59f3667ac
lwyers2 <lwyers2@gmail.com>
2024-10-01 21:14:34 +0100

    Exercise 4.10 and 4.11

--- part4/bloglist/test/blog_api.test.js
+++ part4/bloglist/test/blog_api.test.js
@@ -12,24 +12,24 @@ const initialBlogs =[
         title: "React patterns",
         author: "Michael Chan",
         url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
+        likes: 7
+        
       },
       {
         id: "5a422aa71b54a676234d17f8",
         title: "Go To Statement Considered Harmful",
         author: "Edsger W. Dijkstra",
         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
+        likes: 5
+        
       },
       {
         id: "5a422b3a1b54a676234d17f9",
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
+        likes: 12
+       
       }
 ]
 
@@ -71,6 +71,52 @@ test.only('id field is named id', async () => {
     contents.forEach(e => {
         assert(e.includes('id'))
     })         
+})
+
+test.only('a valid blog can be added', async () => {
+    const newBlog = {
+      id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+    assert(titles.includes('First class tests'))
+})
+
+test.only('a blog with likes field missing returns 0', async () => {
+    const newBlog = {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html"
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    //I am using the title as the identifier here, I could also do the below
+    // const newBlogAdded = response.body[response.body.length -1]
+    const newBlogAdded = response.body.find(r => r.title === newBlog.title)
+
+    assert.strictEqual(newBlogAdded.likes, 0)
 })
 
 after(async () => {
