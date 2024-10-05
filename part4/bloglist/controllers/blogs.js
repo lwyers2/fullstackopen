05eb7c8cd088f929f5e4d92bf09f8583c1cfa6ee
lwyers2 <lwyers2@gmail.com>
2024-10-05 20:00:33 +0100

    Exercise 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,9 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
-
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -15,6 +22,11 @@ blogsRouter.get('/', async (request, response) => {
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if(!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid '})
+    }
+
     let userId
     if(!body.userId) {
       const user = await User.findOne({})
@@ -23,8 +35,7 @@ blogsRouter.get('/', async (request, response) => {
       userId = body.userId
     } 
 
-    const user = await User.findById(userId)
-    console.log(user)
+    const user = await User.findById(decodedToken.id)
 
     if(!body.title || !body.url) {
       return response.status(400).json({error : `url or title missing`})
