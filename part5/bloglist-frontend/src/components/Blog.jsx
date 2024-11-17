4ba226eb39b4d0717cb09d90e5c5d36dc451d195
lwyers2 <lwyers2@gmail.com>
2024-11-17 19:35:26 +0000

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => { 
+  const[showAll, setShowAll] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+  <div style={blogStyle}>
+    {showAll ? (
+      <div>
+       {blog.title} {blog.author}
+       <button onClick={() => setShowAll(false)}>hide</button>
+       <br />
+       {blog.url}
+       <br />
+       likes {blog.likes}
+       <br />
+       {blog.author}
+       </div>
+    ) : (
+      <div>
+      {blog.title} {blog.author}
+      <button onClick={() => setShowAll(true)}>view</button>
+      </div>
+    )}
+    </div>
+  )  
+}
 
 export default Blog
