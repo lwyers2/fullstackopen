efcaf75bbc3eeb84e3bcd7b7f7e25f473afe8bcc
lwyers2 <lwyers2@gmail.com>
2024-12-08 14:47:51 +0000

    Exercise 7.4

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../requests'
+import { useField } from '../hooks'
 
 
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const anecdoteField = useField('text')
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
@@ -20,16 +22,22 @@ const AnecdoteForm = () => {
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    anecdoteField.onChange({ target: { value: ''} })
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
   }
+
+  // const onReset = (event) => {
+  //   event.preventDefault()
+    
+  // }
 
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+      <input {...anecdoteField} />
         <button type="submit">create</button>
       </form>
     </div>
